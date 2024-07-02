c347be7861f4def2777325980a47ba72d9d2729e
Mark Sayson <masayson@gmail.com>
2024-07-01 20:19:06 -0700

    Add required GSI partition/sort key annotations
    Add DynamoDbSecondaryPartitionKey and DynamoDbSecondarySortKey
    annotations to the appropriate DynamoDB table data model to
    enable the DynamoDB enhanced client to generate correct code
    integrating with the ConsentsByServiceUser GSI.
    
    The ConsentsByServiceUser GSI's compound key is the combination of
    user ID and service ID, and we choose user ID as the GSI partition
    key since it will be more well-distributed and hence introduce
    fewer DynamoDB hot-key/throttling issues than service ID, which
    we will take as the GSI sort key.
    
    Before this change, querying the ListServiceUserConsents API
    in AWS API Gateway results in a 500 Internal Service Error with
    error message: "Attempt to execute an operation that requires a
    secondary index without defining the index attributes in the table
    metadata. Index name: ConsentsByServiceUser", and error type
    java.lang.IllegalArgumentException.

--- src/main/java/com/consentframework/consentmanagement/api/infrastructure/entities/DynamoDbServiceUserConsent.java
+++ src/main/java/com/consentframework/consentmanagement/api/infrastructure/entities/DynamoDbServiceUserConsent.java
@@ -6,6 +6,8 @@ import jakarta.annotation.Nullable;
 import org.immutables.value.Value.Immutable;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbImmutable;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondarySortKey;
 
 import java.util.Map;
 
@@ -32,8 +34,21 @@ public interface DynamoDbServiceUserConsent {
     @DynamoDbPartitionKey
     String id();
 
+    /**
+     * The ConsentsByServiceUser GSI, which supports querying for all
+     * consents for a given User or Service-User pair, uses the more
+     * well-distributed "userId" attribute as its partition key to
+     * mitigate hot-key issues when a single service has a large number
+     * of users with different consents, as well as to support retrieving
+     * all consents for a given user across all associated services.
+     *
+     * It uses "serviceId" as its sort key since service IDs are expected
+     * to be less well-distributed for a given hosting company.
+     */
+    @DynamoDbSecondarySortKey(indexNames = { CONSENT_BY_SERVICE_USER_GSI_NAME })
     String serviceId();
 
+    @DynamoDbSecondaryPartitionKey(indexNames = { CONSENT_BY_SERVICE_USER_GSI_NAME })
     String userId();
 
     String consentId();
