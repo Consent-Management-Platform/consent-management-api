80c026469d4f1c73a60904227eb8fc6817608e64
Mark Sayson <masayson@gmail.com>
2025-04-11 23:21:06 -0700

    Add DDB table model attributes for ActiveConsentsWithExpiryTime GSI
    Update the ServiceUserConsent DynamoDB table model to include
    the ActiveConsentsWithExpiryTime GSI attributes.
    
    This GSI will be used to support the consent auto-expiration
    workflow.
    
    Ref: See "Consent auto-expiration" component design in
    https://docs.google.com/document/d/1z-e3ybppuJMujcLjY8FOCvQ2yB4HrFdymSMctr17kBY/

--- src/main/java/com/consentframework/consentmanagement/api/infrastructure/entities/DynamoDbServiceUserConsent.java
+++ src/main/java/com/consentframework/consentmanagement/api/infrastructure/entities/DynamoDbServiceUserConsent.java
@@ -20,6 +20,42 @@ import java.util.Map;
 public interface DynamoDbServiceUserConsent {
     public static final String TABLE_NAME = "ServiceUserConsent";
     public static final String PARTITION_KEY = "id";
+
+    /**
+     * The ActiveConsentsWithExpiryTime GSI supports querying for all active
+     * consents with non-null expiryTime values.
+     *
+     * The GSI partition key, "activeId", is an optional consent attribute
+     * set to the consent's partition key value for active consents with
+     * non-null expiryTime values.
+     *
+     * This makes the GSI a sparse index that only contains consents relevant
+     * for the auto expiration workflow.
+     *
+     * The GSI sort key, "expiryTime", is used to scan in-scope consents
+     * in ascending order of expiry time, to efficiently identify
+     * consents that should be expired.
+     *
+     * The GSI only contains activeId and expiryTime attributes, which
+     * are sufficient to identify consents to expire and submit DynamoDB
+     * UpdateItem requests.
+     */
+    public static final String ACTIVE_CONSENTS_WITH_EXPIRY_TIME_GSI_NAME = "ActiveConsentsWithExpiryTime";
+
+    /**
+     * The ConsentsByServiceUser GSI, which supports querying for all
+     * consents for a given User or Service-User pair, uses the more
+     * well-distributed "userId" attribute as its partition key to
+     * mitigate hot-key issues when a single service has a large number
+     * of users with different consents, as well as to support retrieving
+     * all consents for a given user across all associated services.
+     *
+     * It uses "serviceId" as its sort key since service IDs are expected
+     * to be less well-distributed for a given hosting company.
+     *
+     * The GSI contains all consent attributes, which allows for efficient
+     * retrieval of all consent metadata for a given service user.
+     */
     public static final String CONSENT_BY_SERVICE_USER_GSI_NAME = "ConsentsByServiceUser";
 
     static Builder builder() {
@@ -34,17 +70,10 @@ public interface DynamoDbServiceUserConsent {
     @DynamoDbPartitionKey
     String id();
 
-    /**
-     * The ConsentsByServiceUser GSI, which supports querying for all
-     * consents for a given User or Service-User pair, uses the more
-     * well-distributed "userId" attribute as its partition key to
-     * mitigate hot-key issues when a single service has a large number
-     * of users with different consents, as well as to support retrieving
-     * all consents for a given user across all associated services.
-     *
-     * It uses "serviceId" as its sort key since service IDs are expected
-     * to be less well-distributed for a given hosting company.
-     */
+    @DynamoDbSecondaryPartitionKey(indexNames = { ACTIVE_CONSENTS_WITH_EXPIRY_TIME_GSI_NAME })
+    @Nullable
+    String activeId();
+
     @DynamoDbSecondarySortKey(indexNames = { CONSENT_BY_SERVICE_USER_GSI_NAME })
     String serviceId();
 
@@ -63,6 +92,7 @@ public interface DynamoDbServiceUserConsent {
     @Nullable
     Map<String, String> consentData();
 
+    @DynamoDbSecondarySortKey(indexNames = { ACTIVE_CONSENTS_WITH_EXPIRY_TIME_GSI_NAME })
     @Nullable
     String expiryTime();
 }
